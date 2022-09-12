633ea29339960ae1de577c29a6f7e3146a856172
OscarAlarcon99 <65203702+OscarAlarcon99@users.noreply.github.com>
2022-09-12 00:57:42 -0500

    Assets y movimiento principal

---
+++ PruebaTecnicaOscarAlarcon/Assets/Scripts/Player/SimpleSampleCharacterControl.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSampleCharacterControl : MonoBehaviour
+{
+    /// <summary>
+    /// Variable que almacena la velocidad del movimiento
+    /// </summary>
+    [SerializeField] private float moveSpeed = 2;
+    /// <summary>
+    /// Variable que almacena la fuerza del salto
+    /// </summary>
+    [SerializeField] private float m_jumpForce = 4;
+    /// <summary>
+    /// Controlador de animaciones
+    /// </summary>
+    [SerializeField] private Animator m_animator = null;
+    /// <summary>
+    /// Cuerpo Rigido del player
+    /// </summary>
+    [SerializeField] private Rigidbody m_rigidBody = null;
+    /// <summary>
+    /// Variable que almacena la anterior velocidad vertical
+    /// </summary>
+    private float m_currentV = 0;
+    /// <summary>
+    /// Variable que almacena la anterior velocidad horizontal
+    /// </summary>
+    private float m_currentH = 0;
+
+    private readonly float m_interpolation = 10;
+    private readonly float m_walkScale = 0.33f;
+    /// <summary>
+    /// Variable que almacena si el player toco el suelo
+    /// </summary>
+    private bool m_wasGrounded;
+    /// <summary>
+    /// Variable que almacena vector por donde nos hemos movido
+    /// </summary>
+    private Vector3 m_currentDirection = Vector3.zero;
+    /// <summary>
+    /// Variable que almacena el tiempo inicial del salto
+    /// </summary>
+    private float m_jumpTimeStamp = 0;
+    /// <summary>
+    /// Variable que almacena la variacion minima del intervalo de salto
+    /// </summary>
+    private float m_minJumpInterval = 0.25f;
+    /// <summary>
+    /// Variable que almacena bool estado de si se preciono el inoput encargado de saltar    
+    /// /// </summary>
+    private bool m_jumpInput = false;
+
+    [SerializeField]
+    private bool m_isGrounded;
+
+    private List<Collider> m_collisions = new List<Collider>();
+
+    private void Awake()
+    {
+        if (!m_animator) { gameObject.GetComponent<Animator>(); }
+        if (!m_rigidBody) { gameObject.GetComponent<Animator>(); }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                if (!m_collisions.Contains(collision.collider))
+                {
+                    m_collisions.Add(collision.collider);
+                }
+                m_isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        bool validSurfaceNormal = false;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                validSurfaceNormal = true; break;
+            }
+        }
+
+        if (validSurfaceNormal)
+        {
+            m_isGrounded = true;
+            if (!m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Add(collision.collider);
+            }
+        }
+        else
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+            if (m_collisions.Count == 0) { m_isGrounded = false; }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (m_collisions.Contains(collision.collider))
+        {
+            m_collisions.Remove(collision.collider);
+        }
+        if (m_collisions.Count == 0) { m_isGrounded = false; }
+    }
+
+    private void Update()
+    {
+        if (!m_jumpInput && Input.GetKey(KeyCode.Space))
+        {
+            m_jumpInput = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_animator.SetBool("Grounded", m_isGrounded);
+        DirectUpdate();
+        m_wasGrounded = m_isGrounded;
+        m_jumpInput = false;
+    }
+
+    private void DirectUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Transform camera = Camera.main.transform;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            v *= m_walkScale;
+            h *= m_walkScale;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        if (direction != Vector3.zero)
+        {
+            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+
+            transform.rotation = Quaternion.LookRotation(m_currentDirection);
+            transform.position += m_currentDirection * moveSpeed * Time.deltaTime;
+
+            m_animator.SetFloat("MoveSpeed", direction.magnitude);
+        }
+
+        JumpingAndLanding();
+    }
+
+    private void JumpingAndLanding()
+    {
+        bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+
+        if (jumpCooldownOver && m_isGrounded && m_jumpInput)
+        {
+            m_jumpTimeStamp = Time.time;
+            m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+        }
+
+        if (!m_wasGrounded && m_isGrounded)
+        {
+            m_animator.SetTrigger("Land");
+        }
+
+        if (!m_isGrounded && m_wasGrounded)
+        {
+            m_animator.SetTrigger("Jump");
+        }
+    }
+}
